7e97f46cad35a56b372f34af31b5f64e15c50ec3
NDMO1 <2004ndof@gmail.com>
2022-07-06 21:37:46 +0100

    f

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pygame import mixer
 pygame.init()
 
 # Criar o screen
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((800, 600))
 
 background = pygame.image.load('fundo.jpg')
 
@@ -34,12 +34,12 @@ enemyX_change = []
 enemyY_change = []
 num_of_enemies = 6
 
-for i in range (num_of_enemies):
-   enemyImg.append(pygame.image.load('aircraft.png'))
-   enemyX.append(random.randint(0,735))
-   enemyY.append(random.randint(50,150))
-   enemyX_change.append(4)
-   enemyY_change.append(40)
+for i in range(num_of_enemies):
+    enemyImg.append(pygame.image.load('aircraft.png'))
+    enemyX.append(random.randint(0, 735))
+    enemyY.append(random.randint(50, 150))
+    enemyX_change.append(4)
+    enemyY_change.append(40)
 
 # Tiros
 bulletImg = pygame.image.load('bullet.png')
@@ -50,7 +50,7 @@ bulletY_change = 10
 bullet_state = "ready"
 
 score_value = 0
-font = pygame.font.Font('freesansbold.ttf',32)
+font = pygame.font.Font('freesansbold.ttf', 32)
 
 textX = 10
 textY = 10
@@ -59,32 +59,38 @@ textY = 10
 
 over_font = pygame.font.Font('freesansbold.ttf', 64)
 
-def show_score(x,y):
-    score = font.render("Score : " +str(score_value),True, (255,255,255))
+
+def show_score(x, y):
+    score = font.render("Score : " + str(score_value), True, (255, 255, 255))
     screen.blit(score, (x, y))
+
 
 def game_over_text():
     over_text = font.render("GAME OVER", True, (255, 255, 255))
     screen.blit(over_text, (310, 250))
 
 
-def player(x,y):
+def player(x, y):
     screen.blit(playerImg, (x, y))
 
-def enemy(x,y):
+
+def enemy(x, y):
     screen.blit(enemyImg[i], (x, y))
 
-def fire_bullet(x,y):
+
+def fire_bullet(x, y):
     global bullet_state
     bullet_state = "fire"
-    screen.blit(bulletImg, (x + 16,y + 10))
+    screen.blit(bulletImg, (x + 16, y + 10))
 
-def isCollision(enemyX,enemyY,bulletX,bulletY):
-    distance = math.sqrt((math.pow(enemyX-bulletX,2)) + (math.pow(enemyY-bulletY,2)))
+
+def isCollision(enemyX, enemyY, bulletX, bulletY):
+    distance = math.sqrt((math.pow(enemyX - bulletX, 2)) + (math.pow(enemyY - bulletY, 2)))
     if distance < 27:
         return True
     else:
         return False
+
 
 # Game Loop
 running = True
@@ -98,7 +104,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-       # Teclas
+        # Teclas
         if event.type == pygame.KEYDOWN:
 
             if event.key == pygame.K_LEFT:
@@ -107,21 +113,20 @@ while running:
                 playerX_change = 2
             if event.key == pygame.K_SPACE:
                 if bullet_state == "ready":
-                  bullet_Sound = mixer.Sound('laser.wav')
-                  bullet_Sound.play()
-                  bulletX = playerX
-                  fire_bullet(bulletX, bulletY)
+                    bullet_Sound = mixer.Sound('laser.wav')
+                    bullet_Sound.play()
+                    bulletX = playerX
+                    fire_bullet(bulletX, bulletY)
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
 
-
     playerX += playerX_change
 
-    if playerX<=0:
+    if playerX <= 0:
         playerX = 0
-    elif playerX>=736:
+    elif playerX >= 736:
         playerX = 736
 
     for i in range(num_of_enemies):
@@ -152,20 +157,13 @@ while running:
 
         enemy(enemyX[i], enemyY[i])
 
-
-
-    if bulletY <=0 :
+    if bulletY <= 0:
         bulletY = 480
         bullet_state = "ready"
 
     if bullet_state == "fire":
         fire_bullet(bulletX, bulletY)
         bulletY -= bulletY_change
-
-
-
-
-
 
     player(playerX, playerY)
     show_score(textX, textY)
